91cac5b157ebd3cebba41f067bac120690c04a9e
Patrice Hanne <patrice.hanne@gmail.com>
2024-10-30 18:50:59 +0100

    Exo generateur mot de passe DemanderNombreEntre()

--- generateur_mot_de_passe/Program.cs
+++ generateur_mot_de_passe/Program.cs
@@ -22,7 +22,7 @@
             {
                 // poser question
                 Console.Write(question); // "Longueur du mot de passe souhaité ?"
-                                         // récupérer la reponse
+                // récupérer la reponse
                 string reponse = Console.ReadLine();
 
                 try { int reponseInt = int.Parse(reponse); return reponseInt; }
@@ -32,6 +32,26 @@
                 // gérer l'erreur de conversion
                 // boucler tant qu'on a pas reçu une réponse valide (qui contient que des chiffres)
             }
+        }
+
+        static int DemanderNombreEntre(string question, int min, int max)
+        {
+            while (true)
+            {
+                // nombre = DemanderNombre(question)
+                int nombre = DemanderNombre(question);
+                if ((nombre >= min) && (nombre <= max))
+                {
+                    // valide
+                    return nombre;
+                }
+                else
+                {
+                    Console.WriteLine($"ERREUR :  le nombre doit être compris entre {min} et {max}");
+                }
+            }
+            // si le nombre est bien entre min et max => ERREUR / boucler ...
+            // return 0;
         }
     }
 }
