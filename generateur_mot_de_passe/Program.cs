60da6fbb52eadb1e1594d785bbdd2af2ed89e9b4
Patrice Hanne Expleo <patrice.hanne@expleogroup.com>
2024-10-30 17:47:39 +0100

    Exo generateur mot de passe Init

--- generateur_mot_de_passe/Program.cs
+++ generateur_mot_de_passe/Program.cs
@@ -4,7 +4,40 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("--- Generateur de mot de passe ---");
+
+            // 1 - Demander la longueur du mot de passe (DemanderNombre) int longueur_mot_de_passe = ...
+            int longueurMotDePasse = DemanderNombre("Longueur du mot de passe : ");
+
+            // 2 - Générer un alphabet ex: "abcd1234"
+            // 3 - Comment choisir un caractère aléatoire
+            // 4 - Générer le mot de passe
+            // 5 - Amélioration : choix alphabet
+        }
+
+        static int DemanderNombre(string question)
+        {
+            while (true)
+            {
+                // poser question
+                Console.Write(question); // "Longueur du mot de passe souhaité ?"
+                                         // récupérer la reponse
+                string reponse = Console.ReadLine();
+
+                try
+                {
+                    int reponseInt = int.Parse(reponse);
+                    return reponseInt;
+                }
+                catch
+                {
+                    Console.WriteLine("ERREUR : Vous devez rentrer un nombre");
+                }
+
+                // convertir
+                // gérer l'erreur de conversion
+                // boucler tant qu'on a pas reçu une réponse valide (qui contient que des chiffres)
+            }
         }
     }
 }
