47cc708d16f7816b421728388ed2d5514b48364f
Patrice Hanne <patrice.hanne@gmail.com>
2024-10-31 11:51:54 +0100

    Ajout Rand.Next(alphabet[index])

--- generateur_mot_de_passe/Program.cs
+++ generateur_mot_de_passe/Program.cs
@@ -9,10 +9,20 @@ namespace generateur_mot_de_passe
             Console.WriteLine("--- Generateur de mot de passe ---");
 
             // 1 - Demander la longueur du mot de passe (DemanderNombre) int longueur_mot_de_passe = ...
-            int longueurMotDePasse = outils.DemanderNombrePositifNonNul("Longueur du mot de passe : ");
+            // int longueurMotDePasse = outils.DemanderNombrePositifNonNul("Longueur du mot de passe : ");
 
             // 2 - Générer un alphabet ex: "abcd1234"
             // 3 - Comment choisir un caractère aléatoire
+            string alphabet = "abcd1234";
+            int l = alphabet.Length;
+            Console.WriteLine("longueur: " + l);
+            Console.WriteLine(alphabet[0]); // a
+            Console.WriteLine(alphabet[l-1]); // 4
+            Random rand = new Random();
+            int index = rand.Next(0, l); // Rand exclu les valeur max, donc si on veut entre 1 et 8, il faut mettre l et non pas l-1
+            Console.Write("Aléatoire : ");
+            Console.WriteLine(alphabet[index]);
+
             // 4 - Générer le mot de passe
             // 5 - Amélioration : choix alphabet
         }
