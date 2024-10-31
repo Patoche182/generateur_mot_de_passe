d1002a4f01452095347dab12c94fae74959c175e
Patrice Hanne <patrice.hanne@gmail.com>
2024-10-31 15:50:04 +0100

    Ready

--- generateur_mot_de_passe/Program.cs
+++ generateur_mot_de_passe/Program.cs
@@ -13,15 +13,23 @@ namespace generateur_mot_de_passe
 
             // 2 - Générer un alphabet ex: "abcd1234"
             // 3 - Comment choisir un caractère aléatoire
-            string alphabet = "abcd1234";
-            int l = alphabet.Length;
-            Console.WriteLine("longueur: " + l);
-            Console.WriteLine(alphabet[0]); // a
-            Console.WriteLine(alphabet[l-1]); // 4
+            string minuscules = "abcdefghijklmnopqrstuvwxyz";
+            string alphabet = minuscules;
+            string motDePasse = "";
+
+            int longueurAlphabet = alphabet.Length;
+            Console.WriteLine("longueur: " + longueurAlphabet);
+            Console.WriteLine(alphabet[0]); // => 1er caractère de ma chaine
+            Console.WriteLine(alphabet[longueurAlphabet - 1]); // => dernier caractère de ma chaine
             Random rand = new Random();
-            int index = rand.Next(0, l); // Rand exclu les valeur max, donc si on veut entre 1 et 8, il faut mettre l et non pas l-1
+            int index = rand.Next(0, longueurAlphabet); // Rand exclu les valeur max, donc si on veut entre 1 et 8, il faut mettre l et non pas l-1
             Console.Write("Aléatoire : ");
+
             Console.WriteLine(alphabet[index]);
+
+            
+            motDePasse += alphabet[index];
+            Console.WriteLine(motDePasse);
 
             // 4 - Générer le mot de passe
             // 5 - Amélioration : choix alphabet
