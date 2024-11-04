db1ef7ca19cf068b52ebdb7f6881ddb760eaa5ad
Patrice Hanne <patrice.hanne@gmail.com>
2024-11-04 11:37:27 +0100

    Exo Generateur mot de passe ==> motDePasse += alphabet[index];

--- generateur_mot_de_passe/Program.cs
+++ generateur_mot_de_passe/Program.cs
@@ -9,7 +9,7 @@ namespace generateur_mot_de_passe
             Console.WriteLine("--- Generateur de mot de passe ---");
 
             // 1 - Demander la longueur du mot de passe (DemanderNombre) int longueur_mot_de_passe = ...
-            // int longueurMotDePasse = outils.DemanderNombrePositifNonNul("Longueur du mot de passe : ");
+            int longueurMotDePasse = outils.DemanderNombrePositifNonNul("Longueur du mot de passe : ");
 
             // 2 - Générer un alphabet ex: "abcd1234"
             // 3 - Comment choisir un caractère aléatoire
@@ -22,14 +22,17 @@ namespace generateur_mot_de_passe
             Console.WriteLine(alphabet[0]); // => 1er caractère de ma chaine
             Console.WriteLine(alphabet[longueurAlphabet - 1]); // => dernier caractère de ma chaine
             Random rand = new Random();
-            int index = rand.Next(0, longueurAlphabet); // Rand exclu les valeur max, donc si on veut entre 1 et 8, il faut mettre l et non pas l-1
-            Console.Write("Aléatoire : ");
+            Console.WriteLine("Aléatoire : ");
 
-            Console.WriteLine(alphabet[index]);
-
-            
-            motDePasse += alphabet[index];
-            Console.WriteLine(motDePasse);
+            // Boucler sur longueur MotDePasse
+            for (int i = 0; i < longueurMotDePasse; i++)
+            {
+                int index = rand.Next(0, longueurAlphabet); // Rand exclu les valeur max, donc si on veut entre 1 et 8, il faut mettre l et non pas l-1
+                motDePasse += alphabet[index];
+            }
+            // Console.WriteLine(alphabet[index]);
+            // motDePasse += alphabet[index];
+            Console.WriteLine("Mot de passe : " + motDePasse);
 
             // 4 - Générer le mot de passe
             // 5 - Amélioration : choix alphabet
