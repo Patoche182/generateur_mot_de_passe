692e2a747b836c1797f59fcca8772dc1bdaca2b2
Patrice Hanne <patrice.hanne@gmail.com>
2024-11-05 10:23:04 +0100

    Exo générateur mot de passe OK

--- generateur_mot_de_passe/Program.cs
+++ generateur_mot_de_passe/Program.cs
@@ -12,13 +12,11 @@ namespace generateur_mot_de_passe
             int longueurMotDePasse = outils.DemanderNombrePositifNonNul("Longueur du mot de passe : ");
             Console.WriteLine();
             int choixAlphabet = outils.DemanderNombreEntre("Vous voulez un mot de passe avec : \n" +
-                "1 - minuscule\n" +
-                "2 - minuscule et MAJUSCULE\n" +
-                "3 - caractère et chiffres\n" +
-                "4 - caractère, chiffres et caractère spéciaux\n" +
+                "1 - avec uniquement des minuscules\n" +
+                "2 - avec des minuscules + MAJUSCULES\n" +
+                "3 - avec des caractères spéciaux + chiffres\n" +
+                "4 - avec des minuscules + MAJUSCULES + chiffres + caractère spéciaux\n" +
                 "Votre choix : ", 1, 4);
-
-
 
             // 2 - Générer un alphabet ex: "abcd1234"
             // 3 - Comment choisir un caractère aléatoire
@@ -41,7 +39,7 @@ namespace generateur_mot_de_passe
             else if (choixAlphabet == 2)
                 alphabet = minuscules + majuscules;
             else if (choixAlphabet == 3)
-                alphabet = minuscules + majuscules + chiffres;
+                alphabet = caractereSpeciaux + chiffres;
             else
                 alphabet = minuscules + majuscules + chiffres + caractereSpeciaux;
 
